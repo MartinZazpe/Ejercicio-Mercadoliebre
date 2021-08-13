1f9fa96b9f85a34611fdc73df405499a3fef60a1
MartinZazpe <martinzazpe1@gmail.com>
2021-08-13 03:52:46 +0200

    pushing almost all up to 6, need to improve header

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('Public'));
-
 app.listen(3000, ()=> {
 console.log('servidor funcionando')
 });
 
+
+app.use(express.static(path.join(__dirname,'Public')));
+
+
 app.get('/', (req,res) =>{ 
     res.sendFile(path.join(__dirname, '/Views/index.html'))
     });
+
+    app.get('/register', (req,res) =>{ 
+        res.sendFile(path.join(__dirname, '/Views/register.html'))
+        });
