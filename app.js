01463c1d2baee8d8d1f3a0100930e54db3a19c25
MartinZazpe <martinzazpe1@gmail.com>
2021-08-19 00:16:39 +0200

    actualizando, faltan corregir transiciones y barra de busqueda

--- app.js
+++ app.js
@@ -17,3 +17,8 @@ app.get('/', (req,res) =>{
     app.get('/register', (req,res) =>{ 
         res.sendFile(path.join(__dirname, '/Views/register.html'))
         });
+
+        app.get('/login', (req,res) =>{ 
+            res.sendFile(path.join(__dirname, '/Views/login.html'))
+            });
+    
